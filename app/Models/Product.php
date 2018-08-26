1db5d7765d3118923d1b15f5aeeb231d70601455
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-08-26 12:36:08 -0600

    :sparkles: Add simple structure for products and add basic test

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
---
+++ tests/Unit/Products/ProductTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Products;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Product;
+
+class ProductTest extends TestCase
+{
+    /** @test */
+    public function test_it_uses_the_slug_for_the_route_key_name()
+    {
+        $product = new Product();
+
+        $this->assertEquals($product->getRouteKeyName(), 'slug');
+    }
+}
